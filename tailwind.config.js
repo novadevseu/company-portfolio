f7c99698fcdd4f4156ff241e6f16c8e8604d2910
Deniz Yetim <denizyetim@hotmail.com>
2024-12-23 16:16:08 +0300

    new changes from deniz

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx,ts,tsx}",  // Kendi projenize uygun şekilde düzenleyin
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
